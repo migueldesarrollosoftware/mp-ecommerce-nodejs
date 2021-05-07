7cf5b9c516198af84e1f927783f67f4059034a19
miguel <miguel.ram.dev01@gmail.com>
2021-05-06 19:24:38 -0500

    integrando

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
+const mercadopago = require('mercadopago')
+
+mercadopago.configure({
+    access_token: 'PROD_ACCESS_TOKEN'
+});
+
+
+  
 
 var app = express();
  
@@ -16,6 +24,25 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
+    // Create Preference
+    let preference = {
+        items: [
+        {
+            title: 'Mi producto',
+            unit_price: 100,
+            quantity: 1,
+        }
+        ]
+    };
+    
+    mercadopago.preferences.create(preference)
+    .then(function(response){
+    // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+        global.id = response.body.id;
+    }).catch(function(error){
+        console.log(error);
+    });
+    
     res.render('detail', req.query);
 });
 
