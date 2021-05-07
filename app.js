fa8ce06b79cf1264fa01e8b8c5db391b11ab7600
miguel <miguel.ram.dev01@gmail.com>
2021-05-07 00:53:50 -0500

    integrando

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
-const mercadopago = require('mercadopago')
+const path = require('path')
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
 
+// Agrega credenciales
 mercadopago.configure({
-    access_token: 'PROD_ACCESS_TOKEN'
+  access_token: "APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439"
 });
-
 
   
 
@@ -23,27 +25,71 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/img/:img',function (req, res) {
+
+    res.sendFile(path.join(__dirname,`/assets/${req.params.img}`))
+})
+
 app.get('/detail', function (req, res) {
+
+    let { title,price,unit,img} = req.query 
+
+    console.log(`------------------   title ${title} img ${img} price ${price}`) 
     // Create Preference
-    let preference = {
+    let preference = {        
         items: [
-        {
-            title: 'Mi producto',
-            unit_price: 100,
+            {
+            id:1234,
+            title: title,
+            description:`Dispositivo móvil de Tienda e-commerce`,
+            picture_url:`https://migueldesar-mp-commerce-nodejs.herokuapp.com/img/${img}`,
+            unit_price: parseInt(price),
             quantity: 1,
+            }
+
+        ],
+        external_reference: "miguel.ram.dev01@gmail.com",
+        payer: {
+            phone: {
+                area_code: "52",
+                number: 5549737300
+            },
+            address: {
+                zip_code: "03940",
+                street_name: "Insurgentes Sur",
+                street_number: 1602
+            },
+            email: "test_user_46542185@testuser.com",
+            identification: {
+                number: "22334445",
+                type: "DNI"
+            },
+            name: "Lalo",
+            surname: "Landa",
+        },
+        payment_methods: {
+            excluded_payment_methods: [
+                {
+                    id: "diners"
+                },
+                {
+                    id: "pagoefectivo_atm"
+                },
+
+            ],
+            installments: 6
         }
-        ]
-    };
-    
+    }
     mercadopago.preferences.create(preference)
-    .then(function(response){
+        .then(function(response){
     // Este valor reemplazará el string "<%= global.id %>" en tu HTML
-        global.id = response.body.id;
+        console.log(response)
+        init_point = response.body.init_point 
+        res.render('detail', {title,price,unit,img,init_point});
     }).catch(function(error){
         console.log(error);
     });
-    
-    res.render('detail', req.query);
+
 });
 
 app.listen(port);
